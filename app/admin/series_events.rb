e2595962f7c513dfac0b8b504f623722384a91c5
Creighton Higgins <creighton.higgins@gmail.com>
2019-02-13 17:30:06 -0800

    add/remove round w/ course

--- app/admin/series_events.rb
+++ app/admin/series_events.rb
@@ -1,6 +1,8 @@
 ActiveAdmin.register SeriesEvent, as: "Event" do
   belongs_to :league_series
-  permit_params :title, :event_start_time, :event_end_time, player_events_attributes: [:player_id]
+  permit_params :title, :event_start_time, :event_end_time,
+    player_events_attributes: [:player_id],
+    event_round_attributes: [:id, :course_id, :round_num]
 
   form do |f|
     f.inputs "Series: #{league_series.title}" do
@@ -23,11 +25,33 @@ ActiveAdmin.register SeriesEvent, as: "Event" do
 
   member_action :remove_player, method: :delete do
     player = resource.player_events.find_by(player_id: params[:player_id])
-    player.delete
+    player.destroy!
     redirect_to admin_league_series_event_path(
       league_series_id: params[:league_series_id],
       id: params[:id]
     ), notice: "Removed Player"
+  end
+
+  member_action :add_round, method: :put do
+    cur_round = resource.event_rounds.maximum(:round_num) || 0
+    resource.event_rounds.create!(
+      course_id: params[:course_id],
+      round_num: cur_round + 1
+    )
+
+    redirect_to admin_league_series_event_path(
+      league_series_id: params[:league_series_id],
+      id: params[:id]
+    ), notice: "Added Round"
+  end
+
+  member_action :remove_round, method: :delete do
+    round = resource.event_rounds.find_by(id: params[:event_round_id])
+    round.destroy!
+    redirect_to admin_league_series_event_path(
+      league_series_id: params[:league_series_id],
+      id: params[:id]
+    ), notice: "Removed Round"
   end
 
   show do
@@ -37,6 +61,39 @@ ActiveAdmin.register SeriesEvent, as: "Event" do
           attributes_table_for event do
             row("Start Time", &:event_start_time)
             row("End Time", &:event_end_time)
+          end
+        end
+        panel "Rounds" do
+          div do
+            form action: add_round_admin_league_series_event_path(league_series, resource), method: :post do |_f|
+              select name: :course_id do
+                Course.all.map do |c|
+                  option value: c.id do
+                    c.name
+                  end
+                end
+              end
+              input type: :submit, value: "Add Round"
+              input type: :hidden, name: "_method", value: "put"
+              input type: :hidden, name: "authenticity_token", value: form_authenticity_token.to_s
+            end
+          end
+          table_for event.event_rounds do
+            column :round do |r|
+              r.round_num
+            end
+            column :course do |r|
+              r.course.name
+            end
+            column do |r|
+              link_to "remove",
+                remove_round_admin_league_series_event_path(
+                  league_series, resource,
+                  event_round_id: r.id,
+                  authenticity_token: form_authenticity_token.to_s
+                ),
+                method: :delete
+            end
           end
         end
       end
