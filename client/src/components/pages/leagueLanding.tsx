48c1bf33c03bbb602984309ab9a8d44640d97b2a
Creighton Higgins <creighton.higgins@gmail.com>
2019-02-14 08:51:42 -0800

    add countdown timer to league landing for upcoming events

--- client/src/components/pages/leagueLanding.tsx
+++ client/src/components/pages/leagueLanding.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import moment from 'moment-timezone'
+import countdown from 'countdown'
+require('moment-countdown')
 import { RouteComponentProps } from '@reach/router'
 import { League, EventRound, SeriesEvent } from '../../types/league'
 import TextBox from '../style/textBox'
@@ -10,13 +12,32 @@ import Box from '../style/box';
 const formatDatetime = (time: string, zone: string) =>
   moment.tz(time, zone).format("dddd, MMMM Do YYYY, h:mm a")
 
-const EventInfo = ({ heading, event, timezone }: { heading: string, event: SeriesEvent, timezone: string }) =>
-  <Box mt={16}>
+const EventInfo = ({ heading, event, timezone }: { heading: string, event: SeriesEvent, timezone: string }) => {
+  const { title, event_start_time: start_time, event_end_time: end_time } = event
+  const now = moment()
+  const start = moment(start_time)
+  const end = moment(end_time)
+
+  console.log(`EventInfo: ${title}`)
+  console.log({ now: now.toString(), start: start.toString(), end: end.toString(), now_is_before_start: now.isBefore(start) })
+
+  const initCountdown = now.isBefore(start) ? countdown(start.toDate(), now.toDate()).toString() : null
+  const [eventCountdown, setCountdown] = useState<string | null>(initCountdown)
+  useEffect(() => {
+    if (now.isBefore(start)) {
+      setInterval(() => {
+        setCountdown(countdown(start.toDate()).toString())
+      }, 1000)
+    }
+  }, [])
+
+  return (<Box mt={16}>
     <TextBox lg base>
       {heading}: {event.title}
     </TextBox>
     <TextBox dark><ul>
       <li key="start">Starts {formatDatetime(event.event_start_time, timezone)}</li>
+      {eventCountdown ? (<li key="countdown">{eventCountdown}</li>) : null}
       <li key="ends">Ends {formatDatetime(event.event_end_time, timezone)}</li>
       {event.event_rounds.map((r: EventRound) =>
         <li key={r.round_num}>
@@ -24,7 +45,9 @@ const EventInfo = ({ heading, event, timezone }: { heading: string, event: Serie
         </li>
       )}
     </ul></TextBox>
-  </Box>
+  </Box>)
+
+}
 
 
 interface LeagueLandingProps extends RouteComponentProps<{ league_id: string }> { }
