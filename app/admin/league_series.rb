e8388b04cc2a03ac9f7ec38cfd6169d74dd52d3e
Creighton Higgins <creighton.higgins@gmail.com>
2019-02-13 23:51:51 -0800

    players in series, event

--- app/admin/league_series.rb
+++ app/admin/league_series.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register LeagueSeries do
+  belongs_to :league
   permit_params :title, :date_start, :date_end, :league_id
+
+  member_action :add_player, method: :put do
+    player_params = params.permit(:player_id)
+    resource.series_players.create! player_params
+    redirect_to admin_league_league_series_path(
+      league_series_id: params[:league_series_id],
+      id: params[:id]
+    ), notice: "Added Player"
+  end
 
   show do
     attributes_table do
@@ -13,28 +23,47 @@ ActiveAdmin.register LeagueSeries do
 
     columns do
       column do
-        panel 'Player Standings' do
+        panel "Player Standings" do
+          players_to_add = league.players - league_series.players
+          if !players_to_add.empty?
+            form action: add_player_admin_league_league_series_path(league_series, resource), method: :post do |_f|
+              select name: :player_id do
+                (league.players - league_series.players).map do |p|
+                  option value: p.id do
+                    p.display_name
+                  end
+                end
+              end
+              input name: "authenticity_token", type: :hidden, value: form_authenticity_token.to_s
+              input type: :hidden, name: "_method", value: "put"
+              input type: :submit, value: "Add Player to Series"
+            end
+          else
+            para do
+              "[no players to add]"
+            end
+          end
           table_for league_series.players do
-            column 'Name', :display_name
-            column 'League Number', :player_number
+            column "Name", :display_name
+            column "League Number", :player_number
             column do |player|
-              link_to 'VIEW', [:admin, player]
+              link_to "VIEW", [:admin, player]
             end
           end
         end
       end
 
       column do
-        panel 'Events' do
+        panel "Events" do
           para do
-            link_to 'New Event', new_admin_league_series_event_path(resource)
+            link_to "New Event", new_admin_league_series_event_path(resource)
           end
           table_for league_series.series_events do
             column do |event|
               link_to event.title, admin_league_series_event_path(resource, event)
             end
-            column 'Start', :event_start_time
-            column 'End', :event_end_time
+            column "Start", :event_start_time
+            column "End", :event_end_time
           end
         end
       end
--- app/admin/leagues.rb
+++ app/admin/leagues.rb
@@ -3,6 +3,34 @@
 ActiveAdmin.register League do
   permit_params :name
 
+  show do
+    columns do
+      column do
+        panel "Details" do
+          attributes_table_for league do
+            row("Name", &:name)
+          end
+        end
+        panel "Series" do
+          table_for league.league_series do
+            column do |series|
+              link_to series.title, admin_league_league_series_path(league, series)
+            end
+            column "Start", :date_start
+            column "Ends", :date_end
+          end
+        end
+      end
+      column do
+        panel "Players" do
+          table_for league.players do
+            column(&:display_name)
+          end
+        end
+      end
+    end
+  end
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
